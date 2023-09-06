3710d7da990122fe41cebc822257131023689837
FYFcg <1357862552@qq.com>
2023-09-06 22:11:35 +0800

    update

--- myrc/src/main.rs
+++ myrc/src/main.rs
@@ -1,50 +1,62 @@
-use std::cell::RefCell;
+use std::ops::Deref;
 
 struct MyRc<T> {
-    value: T,
-    count: RefCell<usize>,
+    value: *mut T,
+    count: *mut usize,
 }
 
-impl<T:Clone> MyRc<T> {
+impl<T: Clone> MyRc<T> {
     fn new(value: T) -> Self {
         MyRc {
-            value: value,
-            count: RefCell::new(1),
+            value: Box::into_raw(Box::new(value)),
+            count: Box::into_raw(Box::new(1)),
         }
     }
 
     fn clone(&self) -> Self {
-        *self.count.borrow_mut() += 1;
+        unsafe {
+            *self.count += 1;
+        }
         MyRc {
-            value: self.value.clone(),
-            count: self.count.clone(),
+            value: self.value,
+            count: self.count,
         }
     }
 
     fn get_count(&self) -> usize {
-        *self.count.borrow()
+        unsafe { *self.count }
+    }
+}
+
+impl<T> Deref for MyRc<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.value }
     }
 }
 
 impl<T> Drop for MyRc<T> {
     fn drop(&mut self) {
-        let count = &mut *self.count.borrow_mut();
-        *count -= 1;
-        if *count == 0 {
-            println!("Dropping MyRc");
+        unsafe {
+            *self.count -= 1;
+            if *self.count == 0 {
+                println!("Dropping!");
+                drop(Box::from_raw(self.value));
+                drop(Box::from_raw(self.count));
+            }
         }
     }
 }
 
 fn main() {
     let rc1 = MyRc::new(String::from("hello"));
-    println!("count after creating rc1: {}", rc1.get_count());
-
-    let rc2 = rc1.clone();
-    println!("count after cloning rc1: {}", rc1.get_count());
-
-    drop(rc1);
-    println!("count after dropping rc1: {}", rc2.get_count());
-
-    drop(rc2);
+    println!("rc1 = {}", *rc1);
+    println!("count of rc1 after creating rc1: {}", rc1.get_count());
+    {
+        let rc2 = rc1.clone();
+        println!("rc2 = {}", *rc2);
+        println!("count of rc1 after cloning rc1: {}", rc1.get_count());
+        println!("count of rc2 after cloning rc1: {}", rc2.get_count());
+    }
+    println!("count of rc1 after dropping rc2: {}", rc1.get_count());
 }
